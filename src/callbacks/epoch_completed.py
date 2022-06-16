9264bd9756cd6ee531098553cdfa124e594fa4fb
MaxVanDijck <vandijcksm@gmail.com>
2022-06-16 12:41:49 +1200

    add wandb logging

--- src/callbacks/epoch_completed.py
+++ src/callbacks/epoch_completed.py
@@ -4,7 +4,7 @@ import logging
 from ignite.engine import Events, create_supervised_evaluator
 import ignite.distributed as idist
 import ignite
-
+import wandb
 log = logging.getLogger(__name__)
 
 
@@ -14,7 +14,7 @@ def print_learning_rate(engine):
 
     engine.add_event_handler(Events.EPOCH_COMPLETED, function)
     
-def evaluate_model(engine):
+def evaluate_model(engine, log_to_wandb):
     # Create Evaluator
     evaluator = create_supervised_evaluator(
         engine.state.model,
@@ -22,8 +22,11 @@ def evaluate_model(engine):
         device=idist.device(),
     )
     engine.state.evaluator = evaluator
-    def function(engine):
+    def function(engine, log_to_wandb=log_to_wandb):
         state = engine.state.evaluator.run(engine.state.dataloaders.val)
         if idist.get_rank() == 0:
             log.info(state.metrics)
+            if log_to_wandb:
+                wandb.log(state.metrics)
+                log.info("Logged to Wandb")
     engine.add_event_handler(Events.EPOCH_COMPLETED, function)
--- src/callbacks/started.py
+++ src/callbacks/started.py
@@ -3,10 +3,18 @@ from ignite.engine import Events
 from ignite.handlers import LRScheduler
 from torch.optim.lr_scheduler import StepLR
 
+import wandb
+
 def add_lr_scheduler(engine):
     def function(engine):
         torch_lr_scheduler = StepLR(engine.state.optimizer, step_size=8, gamma=0.1)
         scheduler = LRScheduler(torch_lr_scheduler)
         engine.add_event_handler(Events.EPOCH_COMPLETED, scheduler)
 
+    engine.add_event_handler(Events.STARTED, function)
+
+def init_wandb(engine):
+    def function(engine):
+        wandb.login()
+        wandb.init()
     engine.add_event_handler(Events.STARTED, function)
