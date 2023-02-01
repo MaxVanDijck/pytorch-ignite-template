103459fe2f57ae924f5e78904a8ff96ce9a96a23
MaxVanDijck <vandijcksm@gmail.com>
2023-02-01 20:56:45 +1300

    simplify callbacks

--- src/callbacks/epoch_completed.py
+++ src/callbacks/epoch_completed.py
@@ -1,49 +1,6 @@
 import logging
-import os
-
-import ignite
-import ignite.distributed as idist
-import wandb
-from ignite.engine import Events, create_supervised_evaluator
 
 log = logging.getLogger(__name__)
 
-
-def print_learning_rate(engine):
-    def function(engine):
-        log.info(engine.state.optimizer.param_groups[0]["lr"])
-
-    engine.add_event_handler(Events.EPOCH_COMPLETED, function)
-    
-def evaluate_model(engine, log_to_wandb):
-    # Create Evaluator
-    evaluator = create_supervised_evaluator(
-        engine.state.model,
-        metrics={"accuracy": ignite.metrics.Accuracy(), "loss": ignite.metrics.Loss(engine.state.criterion)},
-        device=idist.device(),
-    )
-    engine.state.evaluator = evaluator
-    def function(engine, log_to_wandb=log_to_wandb):
-        state = engine.state.evaluator.run(engine.state.dataloaders.val)
-        if idist.get_rank() == 0:
-            log.info(state.metrics)
-            if log_to_wandb:
-                wandb.log(state.metrics)
-                log.info("Logged to Wandb")
-    engine.add_event_handler(Events.EPOCH_COMPLETED, function)
-
-
-    # save best 3 models based upon evaluation
-    to_save = {
-        'model': engine.state.model,
-        'engine': engine
-    }
-    handler = ignite.handlers.checkpoint.Checkpoint(
-        to_save, 
-        os.getcwd(),
-        n_saved=3,
-        filename_prefix='best',
-        score_name="accuracy",
-    )
-    log.info(f"Saving best models to: {os.getcwd()}")
-    engine.state.evaluator.add_event_handler(Events.COMPLETED, handler)
+def log_lr(engine):
+    log.info(engine.state.optimizer.param_groups[0]["lr"])
--- src/engines/supervised.py
+++ src/engines/supervised.py
@@ -1,28 +1,29 @@
-import hydra
-from omegaconf import DictConfig
+import logging
+import os
 
+import hydra
+import ignite
+import ignite.distributed as idist
 import torch.nn as nn
+import wandb
+from ignite.contrib.handlers import ProgressBar
+from ignite.engine import Engine, Events, create_supervised_evaluator
+from omegaconf import DictConfig
 from torch.cuda.amp import autocast
 
-import ignite.distributed as idist
-from ignite.engine import Engine, Events
-from ignite.contrib.handlers import ProgressBar
-
+log = logging.getLogger(__name__)
 
 def create_engine(model: nn.Module, config: DictConfig):
     """
-    Combines the model and config into an engine
+    Combines the model and config into an engine containing a train and validation loop
 
-    Any extra objects should be passed to this function such as:
+    Any extra objects should be added to the engine state such as:
     - Optimizers
     - Loss functions
     This is to ensure we can access these objects 
     through callbacks in train_pipeline.py
-
-    WARNING: If we initilize anything other than the engine here, 
-    we will not be able to access it through callbacks
     """
-
+    # TRAIN
     optimizer = idist.auto_optim(hydra.utils.instantiate(config.engine.optimizer, params=model.parameters()))
     criterion = hydra.utils.instantiate(config.engine.criterion).to(idist.device())
     
@@ -53,10 +54,40 @@ def create_engine(model: nn.Module, config: DictConfig):
     engine.state.criterion = criterion
     engine.state.optimizer = optimizer
 
-
     if idist.get_rank() == 0:
         # Add progress bar showing batch loss value
         ProgressBar().attach(engine, output_transform=lambda x: {"batch loss": x})
+
+    # VALIDATION
+    evaluator = create_supervised_evaluator(
+        model,
+        metrics={"accuracy": ignite.metrics.Accuracy(), "loss": ignite.metrics.Loss(engine.state.criterion)},
+        device=idist.device(),
+    )
+    engine.state.evaluator = evaluator
+    def function(engine, log_to_wandb=config.engine.log_to_wandb):
+        state = engine.state.evaluator.run(engine.state.dataloaders.val)
+        if idist.get_rank() == 0:
+            log.info(state.metrics)
+            if log_to_wandb:
+                wandb.log(state.metrics)
+                log.info("Logged to Wandb")
+    engine.add_event_handler(Events.EPOCH_COMPLETED, function)
+
+    # save best 3 models based upon evaluation
+    to_save = {
+        'model': model,
+        'engine': engine
+    }
+    handler = ignite.handlers.checkpoint.Checkpoint(
+        to_save, 
+        os.getcwd(),
+        n_saved=3,
+        filename_prefix='best',
+        score_name="accuracy",
+    )
+    log.info(f"Saving best models to: {os.getcwd()}")
+    engine.state.evaluator.add_event_handler(Events.COMPLETED, handler)
 
     return engine
 
