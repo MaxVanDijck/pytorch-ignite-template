1c63777345f367e51dec4a8573e42cc6d8b34da4
MaxVanDijck <vandijcksm@gmail.com>
2023-02-18 17:52:59 +1300

    download=True

--- src/data/cifar10.py
+++ src/data/cifar10.py
@@ -1,7 +1,8 @@
-from src.components import Datasets
-
 import torchvision
 from torchvision.transforms import Compose, Normalize, ToTensor
+
+from src.components import Datasets
+
 
 class CIFAR10TrainDataset(torchvision.datasets.CIFAR10):
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
@@ -15,7 +16,7 @@ def get_datasets(root):
     transform = Compose([ToTensor(), Normalize((0.485, 0.456, 0.406), (0.229, 0.23, 0.225))])
 
     return Datasets(
-        train = CIFAR10TrainDataset(root, transform=transform),
-        val = CIFAR10ValDataset(root, transform=transform),
+        train = CIFAR10TrainDataset(root, transform=transform, download=True),
+        val = CIFAR10ValDataset(root, transform=transform, download=True),
         test = None
     )
